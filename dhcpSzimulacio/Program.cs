0c845eeb7b602cc7977502a81677299b16f03ff6
FabianTamas <fabiant0130@gmail.com>
2020-10-08 09:24:39 +0200

    CimEggyelNo metódus elkészítése

--- dhcpSzimulacio/Program.cs
+++ dhcpSzimulacio/Program.cs
@@ -3,13 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace dhcpSzimulacio
 {
     class Program
     {
+        static List<string> excluded = new List<string>();
+        static Dictionary<string, string> reserved = new Dictionary<string, string>();
+
+        static void BeolvasExcluded()
+        {
+            try
+            {
+                StreamReader file = new StreamReader("excluded.csv");
+                try
+                {
+                    while (!file.EndOfStream)
+                    {
+                        excluded.Add(file.ReadLine());
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                finally
+                {
+                    file.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static string CimEggyelNo(string cim)
+        {
+            /*
+             * cim = "192.168.10.100"
+             * return "192.168.10.101"
+             * 
+             * szétvágni '.'
+             * az utolsót int-é konvertálni és
+             * egyet hozzáadni (255 ne lépjük túl)
+             * 
+             * összefűzni string-é
+             */
+            string[] adatok = cim.Split('.');
+            int okt4 = Convert.ToInt32(adatok[3]);
+            if (okt4 < 255)
+            {
+                okt4++;
+            }
+            return adatok[0] + "." + adatok[1] + "." + adatok[2] + "." + okt4.ToString();
+        }
+
         static void Main(string[] args)
         {
+            BeolvasExcluded();
+
+            //foreach (var e in excluded)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+            Console.WriteLine(CimEggyelNo("192.168.10.100"));
+
+            Console.WriteLine("\nVége...");
+            Console.ReadLine();
         }
     }
 }
